5e2a357eb97f2157a3a7302fe0de58178fc5663f
dark-wed <muhammadbinfasiraka@gmail.com>
2026-02-26 12:36:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,17 @@ function logout() {
     localStorage.clear();
     window.location.href = "index.html";
 }
+
+// Show username on dashboard
+window.onload = function() {
+    let currentUser = localStorage.getItem("user");
+
+    if(window.location.pathname.includes("dashboard.html")) {
+        if(!currentUser) {
+            window.location.href = "login.html";
+        } else {
+            document.querySelector(".dashboard h1").innerText =
+                "Welcome " + currentUser + " 👋";
+        }
+    }
+};
