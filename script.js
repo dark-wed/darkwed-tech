8acff5289e2458eb8553e7819760499a931b1593
dark-wed <muhammadbinfasiraka@gmail.com>
2026-02-26 12:42:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function signup() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    if(username === "" || password === "") {
+    if (username === "" || password === "") {
         alert("Please fill all fields");
         return;
     }
@@ -18,16 +18,17 @@ function logout() {
     window.location.href = "index.html";
 }
 
-// Show username on dashboard
-window.onload = function() {
+window.onload = function () {
     let currentUser = localStorage.getItem("user");
 
-    if(window.location.pathname.includes("dashboard.html")) {
-        if(!currentUser) {
+    if (window.location.pathname.includes("dashboard.html")) {
+        if (!currentUser) {
             window.location.href = "login.html";
         } else {
-            document.querySelector(".dashboard h1").innerText =
-                "Welcome " + currentUser + " 👋";
+            let welcome = document.getElementById("welcomeText");
+            if (welcome) {
+                welcome.innerText = "Welcome " + currentUser + " 👋";
+            }
         }
     }
 };
